c272797bed0b88b9f4d182c254c78e0ef3067556
Md Khairul Alom <khairul9215@gmail.com>
2023-07-16 01:20:33 +0600

    registration and login

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,10 @@
 
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
 from django.urls import path
+from assignments.models import About
+from .forms import RegistrationForm
+from django.contrib import auth
 
 from blogs.models import Blog, Category
 
@@ -8,9 +12,49 @@ def home(request):
    
     featured_posts=Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
     posts=Blog.objects.filter(is_featured=False, status='Published')
+    try:
+        about= About.objects.get()
+
+    except:
+        about=None
     context={
         
         'featured_posts':featured_posts,
         'posts':posts,
+        'about': about,
     }
     return render(request, 'home.html', context)
+
+def register(request):
+    if request.method=='POST':
+        form=RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    else:
+        form=RegistrationForm()
+    context={
+        'form':form,
+    }
+    return render(request, 'register.html', context)
+
+def login(request):
+    if request.method =="POST":  
+        form=AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user=auth.authenticate(usenrname=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect('home')
+
+    form=AuthenticationForm()
+    context={
+        'form':form,
+    }
+    return render(request, 'login.html',context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
