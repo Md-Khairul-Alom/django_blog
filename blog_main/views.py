b2e62cc6874013c9f2ef0186f45c498e054b992b
Md Khairul Alom <khairul9215@gmail.com>
2023-07-15 15:45:26 +0600

    posts by cetegory

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,5 +2,15 @@
 from django.shortcuts import render
 from django.urls import path
 
+from blogs.models import Blog, Category
+
 def home(request):
-    return render(request, 'home.html')
+   
+    featured_posts=Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
+    posts=Blog.objects.filter(is_featured=False, status='Published')
+    context={
+        
+        'featured_posts':featured_posts,
+        'posts':posts,
+    }
+    return render(request, 'home.html', context)
